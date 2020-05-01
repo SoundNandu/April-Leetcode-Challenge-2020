6d679eca7bdc0ad8b81d34431cd7463af17e6914
Sowndhariya Nandarajkumar <41890348+SoundNandu@users.noreply.github.com>
2020-05-01 14:55:06 -0700

    Create singleNumber.py

---
+++ singleNumber.py
@@ -0,0 +1,34 @@
+Single Number
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+
+
+Time : O(n) Space : O(1)
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums == None or len(nums) == 0:
+            return 
+        for i in range(len(nums)):
+        #Check for the value persent left or right side of the entire array
+            if nums[i] not in nums[i+1:] and nums[i] not in nums[:i]:
+                return nums[i]
